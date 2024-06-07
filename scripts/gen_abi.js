8179aa48c85073359dd8f3564b720b258e028fea
gatsbyter <gatsbyter@gmail.com>
2024-06-07 10:52:55 +0800

    abi

---
+++ scripts/gen_abi.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+
+const Contracts = ["CarvNft", "CarvNftWhitelist", "CarvToken", "veCarvToken", "Settings", "Vault", "ProtocolService"]
+
+function genAbi() {
+    fs.access("./artifacts/contracts", (err) => {
+        if (err) {
+            console.log("run this after compile");
+            return
+        }
+
+        if (!fsExistsSync("./abi")) {
+            fs.mkdirSync("./abi");
+        }
+
+        for (let index in Contracts) {
+            fs.writeFile(
+                "./abi/" + Contracts[index] + ".json",
+                JSON.stringify(require('../artifacts/contracts/' + Contracts[index] + '.sol/' + Contracts[index] + '.json').abi),
+                function (err) {
+                    if (err) {
+                        return console.error(err);
+                    }
+                }
+            );
+        }
+        console.log("success！");
+    });
+}
+
+function fsExistsSync(path) {
+    try{
+        fs.accessSync(path,fs.F_OK);
+    }catch(e){
+        return false;
+    }
+    return true;
+}
+
+genAbi()
