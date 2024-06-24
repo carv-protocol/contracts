f277b6d884e384a59ab41c2719b391356eadb0f0
gatsbyter <gatsbyter@gmail.com>
2024-06-24 09:47:10 +0700

    whitelist script

--- hardhat.config.js
+++ hardhat.config.js
@@ -29,5 +29,10 @@ module.exports = {
       accounts: [process.env.NFT_PRIVATE_KEY],
       gasPrice: 1000000,
     },
+    arb_nft: {
+      url: process.env.API_KEY_URL_ARB_ONE,
+      accounts: [process.env.ARB_NFT_PRIVATE_KEY],
+      gasPrice: 110000000,
+    },
   }
 };
