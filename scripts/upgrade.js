c2c38a896e80d5dbd55a4802e3cfba473a6254de
gatsbyter <gatsbyter@gmail.com>
2024-06-27 10:52:30 +0700

    script

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -2,26 +2,20 @@ const hre = require("hardhat");
 
 const chainID = 421614;
 const proxyAddr = "0x39fc2E18dDdfad008374A527b028fAB8d43845F9";
-const newImplAddr = ""
-const vrfAddr = ""
+const newImplAddr = "0xe01574264B2A330B248AeA2023F86F8BF7750a51"
+const settingAddr = "0x9691cA9635cfa5E32B5F90D155CD9eB6fEf2bFF4"
 
 async function main() {
     const [deployer] = await hre.ethers.getSigners();
 
-    // const ProxyAdmin = await hre.ethers.getContractFactory("ProxyAdmin");
-    // let adminAddr = await hre.upgrades.erc1967.getAdminAddress(proxyAddr)
-    // let proxyAdmin = await ProxyAdmin.attach(adminAddr)
-    // await proxyAdmin.upgradeAndCall(proxyAddr, newImplAddr, hre.ethers.utils.toUtf8Bytes(""))
-
-    // const CarvVrf = await hre.ethers.getContractFactory("CarvVrf");
-    // let vrf = await CarvVrf.attach(vrfAddr)
-    // await vrf.updateVrfConfig({
-    //     keyHash: "0x027f94ff1465b3525f9fc03e9ff7d6d2c0953482246dd6ae07570c45d6631414",
-    //     subId: 339,
-    //     requestConfirmations: 1,
-    //     callbackGasLimit: 1000000,
-    //     numWords: 1,
-    // })
+    const ProxyAdmin = await hre.ethers.getContractFactory("ProxyAdmin");
+    const Proxy = await hre.ethers.getContractFactory("ProtocolService");
+    let adminAddr = await hre.upgrades.erc1967.getAdminAddress(proxyAddr)
+    let proxyAdmin = await ProxyAdmin.attach(adminAddr)
+    let proxy = await Proxy.attach(proxyAddr)
+    let tx = await proxyAdmin.upgradeAndCall(proxyAddr, newImplAddr, hre.ethers.utils.toUtf8Bytes(""))
+    await tx.wait()
+    await proxy.updateSettingsAddress(settingAddr)
 }
 
 main().catch((error) => {
