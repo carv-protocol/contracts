870dc3d16cd295cf001f80b820b7a4533274eaff
gatsbyter <gatsbyter@gmail.com>
2024-07-09 16:51:49 +0700

    oft

--- contracts/CarvToken.sol
+++ contracts/CarvToken.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { OFT } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/OFT.sol";
 
-contract CarvToken is ERC20 {
+contract CarvToken is OFT {
     uint256 constant TOTAL_SUPPLY = 1e9;
 
-    constructor(string memory name, string memory symbol, address receiver) ERC20(name, symbol) {
+    string private constant tokenName = "T-T";
+    string private constant tokenSymbol = "T-T";
+    address private constant lzEndpoint = 0x6EDCE65403992e310A62460808c4b910D972f10f;
+
+    constructor() OFT(tokenName, tokenSymbol, lzEndpoint, msg.sender) Ownable(msg.sender) {
         // one billion CARV Token
-        _mint(receiver, TOTAL_SUPPLY * 10**decimals());
+        _mint(msg.sender, TOTAL_SUPPLY * 10**decimals());
     }
 }
