0cc7d60f9106fac345579a254a3e4cdbb339b2e2
gatsbyter <gatsbyter@gmail.com>
2024-08-24 11:50:58 +0800

    version

--- contracts/interfaces/ICarvVrfCallback.sol
+++ contracts/interfaces/ICarvVrfCallback.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 interface ICarvVrfCallback {
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) external;
--- contracts/test/GenerateOptions.sol
+++ contracts/test/GenerateOptions.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 import { OptionsBuilder } from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/libs/OptionsBuilder.sol";
 
