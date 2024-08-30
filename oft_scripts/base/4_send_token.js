4b3bbb64916e767a704dd5bfe28f312af5f1ba96
gatsbyter <gatsbyter@gmail.com>
2024-08-30 11:42:17 +0800

    OFT: base mainnet support

---
+++ oft_scripts/base/4_send_token.js
@@ -0,0 +1,48 @@
+const hre = require("hardhat");
+
+const tokenAddr = ""
+
+const receiver = "0x000000000000000000000000Cfce99eE8630fe51974c9a94f1d9153e9F656E81"
+const ArbitrumEid = 30110
+
+async function main() {
+    const [deployer] = await hre.ethers.getSigners();
+
+    const CarvToken = await hre.ethers.getContractFactory("CarvToken");
+    let token = await CarvToken.attach(tokenAddr);
+
+    let tx
+
+    tx = await token.quoteSend({
+        dstEid: ArbitrumEid,
+        to: receiver,
+        amountLD: e18(10),
+        minAmountLD: e18(10),
+        extraOptions: "0x",
+        composeMsg: "0x",
+        oftCmd: "0x",
+    }, false)
+
+    tx = await token.send({
+        dstEid: ArbitrumEid,
+        to: receiver,
+        amountLD: e18(10),
+        minAmountLD: e18(10),
+        extraOptions: "0x",
+        composeMsg: "0x",
+        oftCmd: "0x",
+    }, {
+        nativeFee: tx.nativeFee,
+        lzTokenFee: 0
+    }, deployer.address, {value: tx.nativeFee})
+    await tx.wait()
+}
+
+function e18(x) {
+    return hre.ethers.BigNumber.from("1000000000000000000").mul(x)
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
