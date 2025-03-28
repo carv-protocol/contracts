0faed4b4218a0b2fe7abbdb51573f293c42cbf79
gatsbyter <gatsbyter@gmail.com>
2025-03-28 17:21:19 +0800

    veCARV(si)

--- contracts/staking/veCarvsi.sol
+++ contracts/staking/veCarvsi.sol
@@ -7,8 +7,16 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract veCarvsi is AccessControlUpgradeable, MulticallUpgradeable {
 
+    struct Position {
+        uint256 amount;
+        uint256 startAt;
+        address receiver;
+    }
+
     bytes32 public constant DEPOSIT_ROLE = keccak256("DEPOSIT_ROLE");
     bytes32 public constant WITHDRAW_ROLE = keccak256("WITHDRAW_ROLE");
+    uint256 public constant ONE_YEAR = 365 days;
+    uint256 public constant APR_PRECISION = 10000;
 
     /*---------- token infos ----------*/
     address public token;
@@ -17,10 +25,13 @@ contract veCarvsi is AccessControlUpgradeable, MulticallUpgradeable {
     /*---------- User position parameters ----------*/
 
     uint256 public totalAmount;
-    mapping(address => uint256) public positions;
+    uint32 public apr;
+    mapping(address => uint32) public userIndexes;
+    mapping(address => mapping(uint32 => Position)) public positions;
 
-    event Deposit(address indexed user, uint256 amount);
-    event Withdraw(address indexed user, uint256 amount);
+    event AprSet(uint32 apr);
+    event Deposit(address user, address receiver, uint32 positionID, uint256 amount);
+    event Withdraw(address user, uint32 positionID, uint256 amount, uint256 reward);
 
     function initialize(
         string memory name_, string memory symbol_, address carvToken
@@ -34,32 +45,58 @@ contract veCarvsi is AccessControlUpgradeable, MulticallUpgradeable {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function depositFor(address user, uint256 amount) external onlyRole(DEPOSIT_ROLE) {
-        require(user != address(0), "invalid user");
-        require(amount > 0, "invalid amount");
-
-        positions[user] += amount;
-        totalAmount += amount;
-        IERC20(token).transferFrom(msg.sender, address(this), amount);
-        emit Deposit(user, amount);
+    function setApr(uint32 newApr) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        apr = newApr;
+        emit AprSet(newApr);
     }
 
-    function withdrawFor(address user, uint256 amount) external onlyRole(WITHDRAW_ROLE) {
+    function depositFor(address user, address receiver, uint256 amount) external onlyRole(DEPOSIT_ROLE) {
+        require(user != address(0), "invalid user");
+        require(receiver != address(0), "invalid receiver");
         require(amount > 0, "invalid amount");
-        require(positions[user] >= amount, "no enough tokens to withdraw");
 
-        positions[user] -= amount;
-        totalAmount -= amount;
-        IERC20(token).transfer(msg.sender, amount);
-        emit Withdraw(user, amount);
+        userIndexes[user] += 1;
+        positions[user][userIndexes[user]] = Position(amount, block.timestamp, receiver);
+        totalAmount += amount;
+        IERC20(token).transferFrom(msg.sender, address(this), amount);
+        emit Deposit(user, receiver, userIndexes[user], amount);
+    }
+
+    function withdrawFor(address user, uint32 positionID) external onlyRole(WITHDRAW_ROLE) {
+        Position memory position = positions[user][positionID];
+        require(position.amount > 0, "no tokens to withdraw");
+
+        uint256 reward = positionReward(position);
+
+        totalAmount -= position.amount;
+        IERC20(token).transfer(position.receiver, position.amount);
+        delete positions[user][positionID];
+        emit Withdraw(user, positionID, position.amount, reward);
     }
 
     function balanceOf(address user) external view returns (uint256) {
-        return positions[user];
+        uint256 totalBalance;
+        for (uint32 index = 1; index <= userIndexes[user]; index++) {
+            totalBalance += positions[user][index].amount;
+        }
+        return totalBalance;
     }
 
     function totalSupply() external view returns (uint256) {
         return totalAmount;
+    }
+
+    function rewardOf(address user) public view returns (uint256) {
+        uint256 totalReward;
+        for (uint32 index = 1; index <= userIndexes[user]; index++) {
+            totalReward += positionReward(positions[user][index]);
+        }
+        return totalReward;
+    }
+
+    function positionReward(Position memory position) internal view returns (uint256) {
+        uint256 duration = block.timestamp - position.startAt;
+        return duration * uint256(apr) * position.amount / (APR_PRECISION * ONE_YEAR);
     }
 
     function decimals() public pure returns (uint8) {
--- test/veCarvsi.js
+++ test/veCarvsi.js
@@ -24,18 +24,35 @@ describe("veCarvsi", function () {
     });
 
     it("deposit", async function () {
-        await expect(veCarvsi.depositFor(alice.address, E18(1000))).to.be.reverted;
+        await veCarvsi.setApr(500)
+
+        await expect(veCarvsi.depositFor(alice.address, alice.address, E18(1000))).to.be.reverted;
         await carv.approve(veCarvsi.address, E18(100000000))
-        await expect(veCarvsi.depositFor(alice.address, E18(1000))).to.be.reverted;
+        await expect(veCarvsi.depositFor(alice.address, alice.address, E18(1000))).to.be.reverted;
         await expect(veCarvsi.grantRole(hre.ethers.utils.keccak256(ethers.utils.toUtf8Bytes("DEPOSIT_ROLE")), owner.address)).not.to.be.reverted
 
-        await expect(veCarvsi.depositFor(alice.address, E18(1000))).not.to.be.reverted;
-        await expect(veCarvsi.depositFor(bob.address, E18(5000))).not.to.be.reverted;
+        await expect(veCarvsi.depositFor(alice.address, alice.address, E18(1000))).not.to.be.reverted;
+        await expect(veCarvsi.depositFor(bob.address, bob.address, E18(5000))).not.to.be.reverted;
 
         console.log(await veCarvsi.balanceOf(alice.address))
         console.log(await veCarvsi.balanceOf(bob.address))
         console.log(await veCarvsi.totalSupply())
 
+        await expect(veCarvsi.depositFor(alice.address, alice.address, E18(6000))).not.to.be.reverted;
+        await expect(veCarvsi.depositFor(bob.address, bob.address, E18(3000))).not.to.be.reverted;
+
+        console.log(await veCarvsi.balanceOf(alice.address))
+        console.log(await veCarvsi.balanceOf(bob.address))
+        console.log(await veCarvsi.totalSupply())
+
+        console.log(await veCarvsi.userIndexes(alice.address))
+        console.log(await veCarvsi.userIndexes(bob.address))
+
+        await time.increase(30 * 24 * 3600);
+        console.log(await veCarvsi.rewardOf(alice.address))
+
+        await time.increase(30 * 24 * 3600 * 5);
+        console.log(await veCarvsi.rewardOf(alice.address))
     });
 
     it("withdraw", async function () {
@@ -43,11 +60,14 @@ describe("veCarvsi", function () {
         await veCarvsi.grantRole(hre.ethers.utils.keccak256(ethers.utils.toUtf8Bytes("DEPOSIT_ROLE")), owner.address)
         await veCarvsi.grantRole(hre.ethers.utils.keccak256(ethers.utils.toUtf8Bytes("WITHDRAW_ROLE")), owner.address)
 
-        await expect(veCarvsi.depositFor(alice.address, E18(1000))).not.to.be.reverted;
+        await expect(veCarvsi.depositFor(alice.address, bob.address, E18(1000))).not.to.be.reverted;
+        await expect(veCarvsi.depositFor(alice.address, bob.address, E18(6000))).not.to.be.reverted;
 
-        await expect(veCarvsi.withdrawFor(alice.address, E18(1000))).not.to.be.reverted;
+        await expect(veCarvsi.withdrawFor(alice.address, 1)).not.to.be.reverted;
+        await expect(veCarvsi.withdrawFor(alice.address, 1)).to.be.reverted;
 
-        await expect(veCarvsi.withdrawFor(alice.address, E18(500))).to.be.reverted;
+        console.log(await veCarvsi.balanceOf(alice.address))
+        console.log(await carv.balanceOf(bob.address))
 
     });
 
