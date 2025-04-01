921a7df590b46213260a38933a731d4636722469
gatsbyter <gatsbyter@gmail.com>
2025-04-01 14:20:48 +0800

    rm reward

--- contracts/staking/veCarvsi.sol
+++ contracts/staking/veCarvsi.sol
@@ -9,14 +9,11 @@ contract veCarvsi is AccessControlUpgradeable, MulticallUpgradeable {
 
     struct Position {
         uint256 amount;
-        uint256 startAt;
         address receiver;
     }
 
     bytes32 public constant DEPOSIT_ROLE = keccak256("DEPOSIT_ROLE");
     bytes32 public constant WITHDRAW_ROLE = keccak256("WITHDRAW_ROLE");
-    uint256 public constant ONE_YEAR = 365 days;
-    uint256 public constant APR_PRECISION = 10000;
 
     /*---------- token infos ----------*/
     address public token;
@@ -25,14 +22,12 @@ contract veCarvsi is AccessControlUpgradeable, MulticallUpgradeable {
     /*---------- User position parameters ----------*/
 
     uint256 public totalAmount;
-    uint32 public apr;
     mapping(address => uint32) public userIndexes;
     mapping(address => mapping(uint32 => Position)) public positions;
 
-    event AprSet(uint32 apr);
     event Deposit(address user, address receiver, uint32 positionID, uint256 amount);
     event ReceiverChanged(address user, uint32 positionID, address receiver);
-    event Withdraw(address user, uint32 positionID, uint256 amount, uint256 reward);
+    event Withdraw(address user, uint32 positionID, uint256 amount);
 
     function initialize(
         string memory name_, string memory symbol_, address carvToken
@@ -46,18 +41,13 @@ contract veCarvsi is AccessControlUpgradeable, MulticallUpgradeable {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function setApr(uint32 newApr) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        apr = newApr;
-        emit AprSet(newApr);
-    }
-
     function depositFor(address user, address receiver, uint256 amount) external onlyRole(DEPOSIT_ROLE) {
         require(user != address(0), "invalid user");
         require(receiver != address(0), "invalid receiver");
         require(amount > 0, "invalid amount");
 
         userIndexes[user] += 1;
-        positions[user][userIndexes[user]] = Position(amount, block.timestamp, receiver);
+        positions[user][userIndexes[user]] = Position(amount, receiver);
         totalAmount += amount;
         IERC20(token).transferFrom(msg.sender, address(this), amount);
         emit Deposit(user, receiver, userIndexes[user], amount);
@@ -76,12 +66,10 @@ contract veCarvsi is AccessControlUpgradeable, MulticallUpgradeable {
         Position memory position = positions[user][positionID];
         require(position.amount > 0, "no tokens to withdraw");
 
-        uint256 reward = positionReward(position);
-
         totalAmount -= position.amount;
         IERC20(token).transfer(position.receiver, position.amount);
         delete positions[user][positionID];
-        emit Withdraw(user, positionID, position.amount, reward);
+        emit Withdraw(user, positionID, position.amount);
     }
 
     function balanceOf(address user) external view returns (uint256) {
@@ -94,19 +82,6 @@ contract veCarvsi is AccessControlUpgradeable, MulticallUpgradeable {
 
     function totalSupply() external view returns (uint256) {
         return totalAmount;
-    }
-
-    function rewardOf(address user) public view returns (uint256) {
-        uint256 totalReward;
-        for (uint32 index = 1; index <= userIndexes[user]; index++) {
-            totalReward += positionReward(positions[user][index]);
-        }
-        return totalReward;
-    }
-
-    function positionReward(Position memory position) internal view returns (uint256) {
-        uint256 duration = block.timestamp - position.startAt;
-        return duration * uint256(apr) * position.amount / (APR_PRECISION * ONE_YEAR);
     }
 
     function decimals() public pure returns (uint8) {
